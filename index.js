6dd1258615178c7942f0552d91ee9d9b4ddfdb65
frenya <frantisek.vymazal@gmail.com>
2016-02-07 12:17:32 +0100

    Reindented files

--- index.js
+++ index.js
@@ -5,33 +5,33 @@
     var stream = require('stream'), 
         util = require('util'),
         Queue = require('./lib/Queue.js'),
-		debug = require('debug')('oms'),
-		ObjectMatcher = require('./matcher');
+        debug = require('debug')('oms'),
+        ObjectMatcher = require('./matcher');
 
     function ObjectMatchingStream(opts) {
-		// Make sure this is used as a constructor
-		if (!this) return new ObjectMatchingStream(opts);
-		
+        // Make sure this is used as a constructor
+        if (!this) return new ObjectMatchingStream(opts);
+
         stream.Duplex.call(this, {objectMode: true});
-        
-		// Setup the object matcher
-		this.matcher = new ObjectMatcher(opts || {});
-		this.matcher.onmatch = this.handleMatch.bind(this);
-		this.matcher.onmiss = this.handleMiss.bind(this);
-		
-		// Setup internal queuing
+
+        // Setup the object matcher
+        this.matcher = new ObjectMatcher(opts || {});
+        this.matcher.onmatch = this.handleMatch.bind(this);
+        this.matcher.onmiss = this.handleMiss.bind(this);
+
+        // Setup internal queuing
         this._queue = new Queue();
         this._eof = false;
         this._queueDrained = false;
-        
+
         this.on('finish', function() {
             debug('Finished writing stuff!!');
-			this.matcher.end();
+            this.matcher.end();
             this._eof = true;
-	        if (this._queueDrained) {
-				this._queueDrained = false;
-            	this._read(1);
-        	}
+            if (this._queueDrained) {
+                this._queueDrained = false;
+                this._read(1);
+            }
         });
     }
 
@@ -39,21 +39,21 @@
 
     ObjectMatchingStream.prototype._write = function (obj, encodingIgnored, done) {
 
-		this.matcher.push(obj);
-		
+        this.matcher.push(obj);
+
         done();
 
     }
 
     ObjectMatchingStream.prototype._read = function (sizeIgnored) {
-        
+
         while(!this._queue.isEmpty()) {
-			var obj = this._queue.dequeue();
-			debug('Pushing', obj);
+            var obj = this._queue.dequeue();
+            debug('Pushing', obj);
             if (!this.push(obj)) {
-            	debug('Output full, number of remaining items in queue is ' + this._queue.getLength());
+                debug('Output full, number of remaining items in queue is ' + this._queue.getLength());
                 return;
-			}
+            }
         }
 
         // Signal End-of-File to consumers if appropriate
@@ -64,30 +64,30 @@
         else this._queueDrained = true;
 
     }
-	
-	ObjectMatchingStream.prototype.handleMatch = function (input, target, distance) {
-		
-		this.enqueueObject({ source: input, target: target, distance: distance });
-		
-	}
-	
-	ObjectMatchingStream.prototype.handleMiss = function (input) {
 
-		this.enqueueObject({ source: input });
-		
-	}
-	    
-	ObjectMatchingStream.prototype.enqueueObject = function (obj) {
-		
-		this._queue.enqueue(obj);
-		
+    ObjectMatchingStream.prototype.handleMatch = function (input, target, distance) {
+
+        this.enqueueObject({ source: input, target: target, distance: distance });
+
+    }
+
+    ObjectMatchingStream.prototype.handleMiss = function (input) {
+
+        this.enqueueObject({ source: input });
+
+    }
+
+    ObjectMatchingStream.prototype.enqueueObject = function (obj) {
+
+        this._queue.enqueue(obj);
+
         if (this._queueDrained) {
             this._queueDrained = false;
             this._read(1);
         }
 
-	}
+    }
 
     module.exports = ObjectMatchingStream;
-    
+
 })();
--- matcher.js
+++ matcher.js
@@ -3,175 +3,175 @@
 
     // Libraries
     var util = require('util'),
-		debug = require('debug')('oms:matcher'),
-		levenshtein = require('fast-levenshtein');
-    
+        debug = require('debug')('oms:matcher'),
+        levenshtein = require('fast-levenshtein');
+
     function ObjectMatcher(opts) {
-		
-		// Make sure this is used as a constructor
-		if (!this) return new ObjectMatcher(opts);
-		
+
+        // Make sure this is used as a constructor
+        if (!this) return new ObjectMatcher(opts);
+
         this.targets = opts.targets || [];
         this.distanceFunction = opts.distanceFunction || defaultDistance;
-		this.maxDistance = opts.maxDistance || Number.MAX_VALUE;
-		
-		debug('Matching ' + this.targets.length + ' items with max distance ' + this.maxDistance);
-		
-		// Local variables
-		this.inputs = [];
-		this.inputDistances = [];
-		this.bestMatches = [];
-		
-		// Callbacks
-		this.onmatch = opts.onmatch || defaultOnMatch;
-		this.onmiss = opts.onmiss || defaultOnMiss;
-        
+        this.maxDistance = opts.maxDistance || Number.MAX_VALUE;
+
+        debug('Matching ' + this.targets.length + ' items with max distance ' + this.maxDistance);
+
+        // Local variables
+        this.inputs = [];
+        this.inputDistances = [];
+        this.bestMatches = [];
+
+        // Callbacks
+        this.onmatch = opts.onmatch || defaultOnMatch;
+        this.onmiss = opts.onmiss || defaultOnMiss;
+
     }
-	
-	ObjectMatcher.prototype.push = function (obj) {
-		
-		debug('Processing ' + obj);
-		
-		var distances = this.calculateTargetDistances(obj);
-		
-		// Check whether we found an identical match or miss completely
-		// in such case, report the item, otherwise add it to the input list
-		// and update best matches
-		var nearestMatch = distances[0];
-		if (!this.reportMatchOrMiss(obj, nearestMatch, true)) {
-			// Update the distance matrix and perform matchup
-			var inputIndex = this.inputs.length;
-			this.inputs.push(obj);
-			this.inputDistances.push(distances);
-			
-			this.findBestMatchForIndex(inputIndex);
-			
-			// debug('Current state');
-			// debug(this.inputDistances);
-		}
-		
-	}
-	
-	ObjectMatcher.prototype.end = function () {
-		
-		for (var i = 0; i < this.inputs.length; i++) {
-			this.reportMatchOrMiss(this.inputs[i], this.inputDistances[i][0], false);
-		}
-		
-	}
+
+    ObjectMatcher.prototype.push = function (obj) {
+
+        debug('Processing ' + obj);
+
+        var distances = this.calculateTargetDistances(obj);
+
+        // Check whether we found an identical match or miss completely
+        // in such case, report the item, otherwise add it to the input list
+        // and update best matches
+        var nearestMatch = distances[0];
+        if (!this.reportMatchOrMiss(obj, nearestMatch, true)) {
+            // Update the distance matrix and perform matchup
+            var inputIndex = this.inputs.length;
+            this.inputs.push(obj);
+            this.inputDistances.push(distances);
+
+            this.findBestMatchForIndex(inputIndex);
+
+            // debug('Current state');
+            // debug(this.inputDistances);
+        }
+
+    }
+
+    ObjectMatcher.prototype.end = function () {
+
+        for (var i = 0; i < this.inputs.length; i++) {
+            this.reportMatchOrMiss(this.inputs[i], this.inputDistances[i][0], false);
+        }
+
+    }
 
     module.exports = ObjectMatcher;
-	
-	// Matching
-	
-	/**
+
+    // Matching
+
+    /**
 	 * Generates an array of tuples (target id, distance) 
 	 * in increasing order of distance.
 	 */
-	ObjectMatcher.prototype.calculateTargetDistances = function(obj) {
+    ObjectMatcher.prototype.calculateTargetDistances = function(obj) {
 
-		var me = this;
-		var distances = this.targets.map(function(target, i) {
-			var dist = me.distanceFunction(obj, target);
-			return (target !== null) ? { index: i, distance: dist } : null;
-		}).filter(function(match) {
-      // Don't store invalid or too distant matches
-      if (!match || match.distance > me.maxDistance) return false;
-      
-      var targetBestMatch = me.bestMatches[match.index];
-      return (!targetBestMatch || targetBestMatch.distance > match.distance);
-    }).sort(function(a, b) { 
-			var d1 = a ? a.distance : Number.MAX_VALUE,
-				d2 = b ? b.distance : Number.MAX_VALUE;
-			return d1 - d2;
-		});
-		
-		return distances;
-		
-	}
-	
-	ObjectMatcher.prototype.findBestMatchForIndex = function(inputIndex) {
-		
-		debug(inputIndex + ': ' +  this.inputs[inputIndex]);
-		
-		// These are the distances of this particular input
-		// from all the targets in increasing order of distance.
-		var distances = this.inputDistances[inputIndex];
-		debug('in ' + JSON.stringify(distances));
-		
-		while ((distances.length > 0) && distances[0]) {
-			var dist = distances[0],
-				targetBestMatch = this.bestMatches[dist.index];		// This is the target's best match so far
-			
-			if (!targetBestMatch || targetBestMatch.distance > dist.distance) {
-				// We are a better match, let's update the best matches record
-				this.bestMatches[dist.index] = { index: inputIndex, distance: dist.distance };
-				debug('=> ' + this.targets[dist.index] + '[' + dist.distance + ']');
+        var me = this;
+        var distances = this.targets.map(function(target, i) {
+            var dist = me.distanceFunction(obj, target);
+            return (target !== null) ? { index: i, distance: dist } : null;
+        }).filter(function(match) {
+            // Don't store invalid or too distant matches
+            if (!match || match.distance > me.maxDistance) return false;
 
-				// Recursively find best match for the input we just replaced
-				if (targetBestMatch) this.findBestMatchForIndex(targetBestMatch.index);
-				
-				return;
-			}
-			
-			// Remove the first item and continue with the next best match
-			distances.shift();
-		}
-		
-	}
-	
-	ObjectMatcher.prototype.reportMatchOrMiss = function(obj, nearestMatch, reportOnlyIdenticalAndRemoveThem) {
-		
-		if (!nearestMatch || nearestMatch.distance > this.maxDistance) {
-			// Report the miss
-			this.onmiss(obj);
-		}
-		else if ((nearestMatch.distance === 0) || !reportOnlyIdenticalAndRemoveThem) {
-			// Report the match
-			this.onmatch(obj, this.targets[nearestMatch.index], nearestMatch.distance);
-			
-			// When reporting identical matches, remove them from targets
-			// so that no target can be matched twice
-			if (reportOnlyIdenticalAndRemoveThem) { 
-        delete this.targets[nearestMatch.index];
-        
-        
-        // We might be replacing an existing match, so let's rematch it
-        var targetBestMatch = this.bestMatches[nearestMatch.index];
-				if (targetBestMatch) this.findBestMatchForIndex(targetBestMatch.index);
-      }
-		}
-		else return false;
-		
-		return true;
-		
-	}
+            var targetBestMatch = me.bestMatches[match.index];
+            return (!targetBestMatch || targetBestMatch.distance > match.distance);
+        }).sort(function(a, b) { 
+            var d1 = a ? a.distance : Number.MAX_VALUE,
+                d2 = b ? b.distance : Number.MAX_VALUE;
+            return d1 - d2;
+        });
 
-	// Supporting methods
-	
-	function defaultDistance(a, b) {
-		
-		if (typeof a === 'number' && typeof b === 'number') return Math.abs(a - b);
-		else if (typeof a === 'string' && typeof b === 'string') return levenshtein.get(a, b);
-		else return null;
-		
-	}
-	
-	function defaultOnMatch(input, target, distance) {
-		
-		if (distance === 0) {
-			console.log(input, ' === ', target);	
-		}
-		else {
-			console.log(input, ' ~' + distance + '~ ', target);
-		}
-		
-	}
-	
-	function defaultOnMiss(input) {
-		
-		console.log(input, ' UNMATCHED');	
-		
-	}
-	    
+        return distances;
+
+    }
+
+    ObjectMatcher.prototype.findBestMatchForIndex = function(inputIndex) {
+
+        debug(inputIndex + ': ' +  this.inputs[inputIndex]);
+
+        // These are the distances of this particular input
+        // from all the targets in increasing order of distance.
+        var distances = this.inputDistances[inputIndex];
+        debug('in ' + JSON.stringify(distances));
+
+        while ((distances.length > 0) && distances[0]) {
+            var dist = distances[0],
+                targetBestMatch = this.bestMatches[dist.index];		// This is the target's best match so far
+
+            if (!targetBestMatch || targetBestMatch.distance > dist.distance) {
+                // We are a better match, let's update the best matches record
+                this.bestMatches[dist.index] = { index: inputIndex, distance: dist.distance };
+                debug('=> ' + this.targets[dist.index] + '[' + dist.distance + ']');
+
+                // Recursively find best match for the input we just replaced
+                if (targetBestMatch) this.findBestMatchForIndex(targetBestMatch.index);
+
+                return;
+            }
+
+            // Remove the first item and continue with the next best match
+            distances.shift();
+        }
+
+    }
+
+    ObjectMatcher.prototype.reportMatchOrMiss = function(obj, nearestMatch, reportOnlyIdenticalAndRemoveThem) {
+
+        if (!nearestMatch || nearestMatch.distance > this.maxDistance) {
+            // Report the miss
+            this.onmiss(obj);
+        }
+        else if ((nearestMatch.distance === 0) || !reportOnlyIdenticalAndRemoveThem) {
+            // Report the match
+            this.onmatch(obj, this.targets[nearestMatch.index], nearestMatch.distance);
+
+            // When reporting identical matches, remove them from targets
+            // so that no target can be matched twice
+            if (reportOnlyIdenticalAndRemoveThem) { 
+                delete this.targets[nearestMatch.index];
+
+
+                // We might be replacing an existing match, so let's rematch it
+                var targetBestMatch = this.bestMatches[nearestMatch.index];
+                if (targetBestMatch) this.findBestMatchForIndex(targetBestMatch.index);
+            }
+        }
+        else return false;
+
+        return true;
+
+    }
+
+    // Supporting methods
+
+    function defaultDistance(a, b) {
+
+        if (typeof a === 'number' && typeof b === 'number') return Math.abs(a - b);
+        else if (typeof a === 'string' && typeof b === 'string') return levenshtein.get(a, b);
+        else return null;
+
+    }
+
+    function defaultOnMatch(input, target, distance) {
+
+        if (distance === 0) {
+            console.log(input, ' === ', target);	
+        }
+        else {
+            console.log(input, ' ~' + distance + '~ ', target);
+        }
+
+    }
+
+    function defaultOnMiss(input) {
+
+        console.log(input, ' UNMATCHED');	
+
+    }
+
 })();
