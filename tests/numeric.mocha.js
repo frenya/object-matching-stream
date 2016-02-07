62a2e99f3659148d60fcb8df0ebe911a29e15876
frenya <frantisek.vymazal@gmail.com>
2016-02-07 14:50:19 +0100

    Fixed indentation

--- tests/numeric.mocha.js
+++ tests/numeric.mocha.js
@@ -7,119 +7,119 @@ var inputData = [914,925,934,330,116,46,834,328,594,852,416,49,385,766,801,746,2
 
 describe('NumericMatching', function() {
 
-  describe('Matching empty array', function() {
-    
-    it('should work without parameters', function(done) {
-      // NOTE: Omitting the `new` keyword here intentionally
-      var oms = Oms();
-      
-      // Test pipeline
-      StreamTest.fromObjects(inputData.slice(0))
-      .pipe(oms)
-      .pipe(StreamTest.toObjects(function(err, objects) {
-        // console.log(err, objects);
-        assert(!err);
-        assert(objects);
-        assert(objects.length === inputData.length);
-        objects.forEach(function(obj, index) {
-          assert.strictEqual(obj.source, inputData[index]);
-          assert(obj.target === undefined);
-          assert(obj.distance === undefined);
+    describe('Matching empty array', function() {
+
+        it('should work without parameters', function(done) {
+            // NOTE: Omitting the `new` keyword here intentionally
+            var oms = Oms();
+
+            // Test pipeline
+            StreamTest.fromObjects(inputData.slice(0))
+                .pipe(oms)
+                .pipe(StreamTest.toObjects(function(err, objects) {
+                // console.log(err, objects);
+                assert(!err);
+                assert(objects);
+                assert.equal(objects.length, inputData.length);
+                objects.forEach(function(obj, index) {
+                    assert.strictEqual(obj.source, inputData[index]);
+                    assert(obj.target === undefined);
+                    assert(obj.distance === undefined);
+                });
+                done();
+            }));
         });
-        done();
-      }));
+
     });
-	  
-  });
-  
-  describe('Matching without max distance', function() {
-    
-    it('should match closest items', function(done) {
-      var oms = new Oms({
-        targets: testTargets.slice(0)
-      });
-      
-      // Test pipeline
-      StreamTest.fromObjects(inputData.slice(0))
-      .pipe(oms)
-      .pipe(StreamTest.toObjects(function(err, objects) {
-        // console.log(err, objects);
-        assert(!err);
-        assert(objects);
-        assert(objects.length === inputData.length);
-        assert.deepStrictEqual(objects, [ 
-          { source: 925, target: 925, distance: 0 },
-          { source: 852 },
-          { source: 385 },
-          { source: 766 },
-          { source: 801 },
-          { source: 746 },
-          { source: 925 },
-          { source: 683 }, 
-          { source: 914 },
-          { source: 934, target: 1000, distance: 66 },
-          { source: 330 },
-          { source: 116, target: 100, distance: 16 },
-          { source: 46 },
-          { source: 834 },
-          { source: 328 },
-          { source: 594 },
-          { source: 416, target: 500, distance: 84 },
-          { source: 49, target: 50, distance: 1 },
-          { source: 243, target: 200, distance: 43 },
-          { source: 20, target: 0, distance: 20 }
-        ]);
-        done();
-      }));
+
+    describe('Matching without max distance', function() {
+
+        it('should match closest items', function(done) {
+            var oms = new Oms({
+                targets: testTargets.slice(0)
+            });
+
+            // Test pipeline
+            StreamTest.fromObjects(inputData.slice(0))
+                .pipe(oms)
+                .pipe(StreamTest.toObjects(function(err, objects) {
+                // console.log(err, objects);
+                assert(!err);
+                assert(objects);
+                assert.equal(objects.length, inputData.length);
+                assert.deepEqual(objects, [ 
+                    { source: 925, target: 925, distance: 0 },
+                    { source: 852 },
+                    { source: 385 },
+                    { source: 766 },
+                    { source: 801 },
+                    { source: 746 },
+                    { source: 925 },
+                    { source: 683 }, 
+                    { source: 914 },
+                    { source: 934, target: 1000, distance: 66 },
+                    { source: 330 },
+                    { source: 116, target: 100, distance: 16 },
+                    { source: 46 },
+                    { source: 834 },
+                    { source: 328 },
+                    { source: 594 },
+                    { source: 416, target: 500, distance: 84 },
+                    { source: 49, target: 50, distance: 1 },
+                    { source: 243, target: 200, distance: 43 },
+                    { source: 20, target: 0, distance: 20 }
+                ]);
+                done();
+            }));
+        });
+
     });
-	  
-  });
-  
-  describe('Matching with max distance', function() {
-    
-    it('should match closest items', function(done) {
-      var oms = new Oms({
-        targets: testTargets.slice(0),
-        maxDistance: 50
-      });
-      
-      // Test pipeline
-      StreamTest.fromObjects(inputData.slice(0))
-      .pipe(oms)
-      .pipe(StreamTest.toObjects(function(err, objects) {
-        // console.log(err, objects);
-        assert(!err);
-        assert(objects);
-        assert(objects.length === inputData.length);
-        assert.deepStrictEqual(objects, [ 
-          { source: 925, target: 925, distance: 0 },
-          { source: 934 },
-          { source: 330 },
-          { source: 834 },
-          { source: 328 },
-          { source: 594 },
-          { source: 852 },
-          { source: 416 },
-          { source: 385 },
-          { source: 766 },
-          { source: 801 },
-          { source: 746 },
-          { source: 925 },
-          { source: 683 },
-          { source: 914 },
-          { source: 116, target: 100, distance: 16 },
-          { source: 46 },
-          { source: 49, target: 50, distance: 1 },
-          { source: 243, target: 200, distance: 43 },
-          { source: 20, target: 0, distance: 20 }
-        ]);
-        done();
-      }));
+
+    describe('Matching with max distance', function() {
+
+        it('should match closest items', function(done) {
+            var oms = new Oms({
+                targets: testTargets.slice(0),
+                maxDistance: 50
+            });
+
+            // Test pipeline
+            StreamTest.fromObjects(inputData.slice(0))
+                .pipe(oms)
+                .pipe(StreamTest.toObjects(function(err, objects) {
+                // console.log(err, objects);
+                assert(!err);
+                assert(objects);
+                assert(objects.length === inputData.length);
+                assert.deepEqual(objects, [ 
+                    { source: 925, target: 925, distance: 0 },
+                    { source: 934 },
+                    { source: 330 },
+                    { source: 834 },
+                    { source: 328 },
+                    { source: 594 },
+                    { source: 852 },
+                    { source: 416 },
+                    { source: 385 },
+                    { source: 766 },
+                    { source: 801 },
+                    { source: 746 },
+                    { source: 925 },
+                    { source: 683 },
+                    { source: 914 },
+                    { source: 116, target: 100, distance: 16 },
+                    { source: 46 },
+                    { source: 49, target: 50, distance: 1 },
+                    { source: 243, target: 200, distance: 43 },
+                    { source: 20, target: 0, distance: 20 }
+                ]);
+                done();
+            }));
+        });
+
     });
-	  
-  });
-  
-  /*
+
+    /*
   StreamTest.versions.forEach(function(version) {
     describe('for ' + version + ' streams', function() {
 
@@ -133,7 +133,7 @@ describe('NumericMatching', function() {
           assert.deepEqual(buffers, expectedBuffers);
           done();
         });
-        
+
         inputStream.pipe(outputStream);
       });
 
@@ -161,7 +161,7 @@ describe('NumericMatching', function() {
           assert.deepEqual(buffers, expectedBuffers.join(''));
           done();
         });
-        
+
         inputStream.pipe(outputStream);
       });
 
@@ -176,7 +176,7 @@ describe('NumericMatching', function() {
           assert(!buffers);
           done();
         });
-        
+
         inputStream.pipe(outputStream);
       });
 
@@ -194,7 +194,7 @@ describe('NumericMatching', function() {
           assert.deepEqual(objs, expectedObjs);
           done();
         });
-        
+
         inputStream.pipe(outputStream);
       });
 
@@ -213,7 +213,7 @@ describe('NumericMatching', function() {
           assert(!objs);
           done();
         });
-        
+
         inputStream.pipe(outputStream);
       });
 
@@ -221,6 +221,6 @@ describe('NumericMatching', function() {
 
   });
   */
-  
+
 });
 
